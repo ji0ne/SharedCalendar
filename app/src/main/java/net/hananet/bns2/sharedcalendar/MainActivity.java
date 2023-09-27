d44a12d64618f4fb2e9f00695dfa28c8194b20ed
ji0ne <zi0neeeeeeeee@gmail.com>
2023-09-27 19:53:56 +0900

    add all file

---
+++ app/src/main/java/net/hananet/bns2/sharedcalendar/MainActivity.java
@@ -0,0 +1,14 @@
+package net.hananet.bns2.sharedcalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
