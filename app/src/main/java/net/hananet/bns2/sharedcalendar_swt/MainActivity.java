24881fd465d74348044cd7889c0d0287f8d91b47
ji0ne <zi0neeeeeeeee@gmail.com>
2024-01-12 06:15:33 +0900

    settings

---
+++ app/src/main/java/net/hananet/bns2/sharedcalendar_swt/MainActivity.java
@@ -0,0 +1,14 @@
+package net.hananet.bns2.sharedcalendar_swt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
